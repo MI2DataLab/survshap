8546acb6d9001162c3979eb35ce5c6c63e339a63
krzyzinskim <mateusz_krzyzinski@wp.pl>
2023-10-25 17:08:58 +0200

    colors for model plot

--- survshap_package/survshap/model_explanations/plot.py
+++ survshap_package/survshap/model_explanations/plot.py
@@ -6,6 +6,7 @@ import plotly.graph_objects
 from .utils import calculate_risk_table, create_boxplot_with_outliers
 from matplotlib import cm
 from matplotlib.colors import LinearSegmentedColormap, Normalize
+import itertools
 
 
 def tooltip_text(row):
@@ -51,20 +52,18 @@ def model_plot_mean_abs_shap_values(
 ):
     df_prepared_to_plot = result.copy()
     if kind == "ratio":
-        df_prepared_to_plot.iloc[:, 5:] = (
-            df_prepared_to_plot.iloc[:, 5:]
-            / df_prepared_to_plot.iloc[:, 5:].abs().sum()
-        )
+        df_prepared_to_plot.iloc[:, 5:] = df_prepared_to_plot.iloc[:, 5:] / df_prepared_to_plot.iloc[:, 5:].abs().sum()
     if variables is not None:
-        df_prepared_to_plot = df_prepared_to_plot[
-            df_prepared_to_plot["variable_name"].isin(variables)
-        ]
+        df_prepared_to_plot = df_prepared_to_plot[df_prepared_to_plot["variable_name"].isin(variables)]
     else:
-        df_prepared_to_plot = df_prepared_to_plot.iloc[:max_vars]
+        df_prepared_to_plot = (
+            df_prepared_to_plot.sort_values("aggregated_change", ascending=False)
+            .iloc[:max_vars]
+            .reset_index(drop=True)
+            .copy()
+        )
 
-    fig = plotly.subplots.make_subplots(
-        rows=2, cols=1, print_grid=False, shared_xaxes=True
-    )
+    fig = plotly.subplots.make_subplots(rows=2, cols=1, print_grid=False, shared_xaxes=True)
     max_y, min_y = -1, 1
     fig.add_trace(
         plotly.graph_objs.Scatter(
@@ -73,9 +72,7 @@ def model_plot_mean_abs_shap_values(
             mode="lines",
             line_color="#4378bf",
             line_width=3,
-            hovertemplate="<b>Average SF</b><br>"
-            + "Time: %{x}<br>"
-            + "<extra></extra>",
+            hovertemplate="<b>Average SF</b><br>" + "Time: %{x}<br>" + "<extra></extra>",
             hoverinfo="text",
         ),
         row=1,
@@ -83,6 +80,9 @@ def model_plot_mean_abs_shap_values(
     )
 
     keyword = "SHAP value: " if kind == "default" else "normalized SHAP value: "
+
+    colors = ["#46bac2", "#ae2c87", "#ffa58c", "#8bdcbe", "#f05a71", "#FED61E", "#FED61E"]
+    colors = itertools.cycle(colors)
 
     for index, row in df_prepared_to_plot.iterrows():
         y_vals = row[5:].values
@@ -92,7 +92,7 @@ def model_plot_mean_abs_shap_values(
                 x=timestamps,
                 y=y_vals,
                 mode="lines",
-                line_color="#4378bf",
+                line_color=next(colors),
                 line_width=2,
                 hovertemplate="<b>"
                 + str(row["variable_name"])
@@ -114,15 +114,11 @@ def model_plot_mean_abs_shap_values(
     if x_range is None:
         x_range = [min(timestamps) - max(timestamps) * 0.025, max(timestamps) * 1.025]
 
-    ticker = matplotlib.ticker.MaxNLocator(
-        nbins=10, min_n_ticks=4, integer=True, prune="upper"
-    )
+    ticker = matplotlib.ticker.MaxNLocator(nbins=10, min_n_ticks=4, integer=True, prune="upper")
     ticks = ticker.tick_values(x_range[0], x_range[1])
 
     if show_risk_table:
-        n_at_risk, n_events, n_censored = calculate_risk_table(
-            ticks, event_times, event_inds
-        )
+        n_at_risk, n_events, n_censored = calculate_risk_table(ticks, event_times, event_inds)
         fig.append_trace(
             plotly.graph_objs.Scatter(
                 x=ticks,
@@ -264,28 +260,19 @@ def model_plot_shap_lines_for_all_individuals(
     df_prepared_to_plot = full_result[full_result["variable_name"] == variable]
     df_prepared_to_plot = df_prepared_to_plot[df_prepared_to_plot["B"] == 0]
     if kind == "ratio":
-        df_prepared_to_plot.iloc[:, 6:] = (
-            df_prepared_to_plot.iloc[:, 6:]
-            / df_prepared_to_plot.iloc[:, 6:].abs().sum()
-        )
+        df_prepared_to_plot.iloc[:, 6:] = df_prepared_to_plot.iloc[:, 6:] / df_prepared_to_plot.iloc[:, 6:].abs().sum()
     df_prepared_to_plot = df_prepared_to_plot.reset_index(drop=True)
     if ~boxplot:
-        cmap = LinearSegmentedColormap.from_list(
-            "dalex", ["#c7f5bf", "#46bac2", "#371ea3"], N=100
-        )
+        cmap = LinearSegmentedColormap.from_list("dalex", ["#c7f5bf", "#46bac2", "#371ea3"], N=100)
         minima = np.min(df_prepared_to_plot["variable_value"])
         maxima = np.max(df_prepared_to_plot["variable_value"])
         norm = matplotlib.colors.Normalize(vmin=minima, vmax=maxima, clip=True)
         mapper = cm.ScalarMappable(norm=norm, cmap=cmap)
         mapped = mapper.to_rgba(df_prepared_to_plot["variable_value"])
-        mapped = np.apply_along_axis(
-            lambda x: matplotlib.colors.rgb2hex(x[:3]), 1, mapped
-        )
+        mapped = np.apply_along_axis(lambda x: matplotlib.colors.rgb2hex(x[:3]), 1, mapped)
         df_prepared_to_plot["index"] = mapped
 
-    fig = plotly.subplots.make_subplots(
-        rows=2, cols=1, print_grid=False, shared_xaxes=True
-    )
+    fig = plotly.subplots.make_subplots(rows=2, cols=1, print_grid=False, shared_xaxes=True)
 
     max_y, min_y = -1, 1
 
@@ -364,15 +351,11 @@ def model_plot_shap_lines_for_all_individuals(
     if x_range is None:
         x_range = [min(timestamps) - max(timestamps) * 0.025, max(timestamps) * 1.025]
 
-    ticker = matplotlib.ticker.MaxNLocator(
-        nbins=10, min_n_ticks=4, integer=True, prune="upper"
-    )
+    ticker = matplotlib.ticker.MaxNLocator(nbins=10, min_n_ticks=4, integer=True, prune="upper")
     ticks = ticker.tick_values(x_range[0], x_range[1])
 
     if show_risk_table:
-        n_at_risk, n_events, n_censored = calculate_risk_table(
-            ticks, event_times, event_inds
-        )
+        n_at_risk, n_events, n_censored = calculate_risk_table(ticks, event_times, event_inds)
         fig.append_trace(
             plotly.graph_objs.Scatter(
                 x=ticks,
@@ -449,251 +432,6 @@ def model_plot_shap_lines_for_all_individuals(
         },
         xaxis2={"visible": False},
     )
-    if kind == "ratio":
-        fig.update_layout(
-            yaxis1={"domain": [0.35, 1], "title": "normalized SHAP value"},
-        )
-    else:
-        fig.update_layout(
-            yaxis1={"domain": [0.35, 1], "title": "SHAP value"},
-        )
-
-    fig.update_layout(
-        title_text=title,
-        title_x=0.15,
-        font={"color": "#371ea3"},
-        template="none",
-        margin={"t": 78, "b": 71, "r": 30},
-    )
-
-    if show:
-        fig.show(
-            config={
-                "displaylogo": False,
-                "staticPlot": False,
-                "toImageButtonOptions": {
-                    "height": None,
-                    "width": None,
-                },
-                "modeBarButtonsToRemove": [
-                    "sendDataToCloud",
-                    "lasso2d",
-                    "autoScale2d",
-                    "select2d",
-                    "zoom2d",
-                    "pan2d",
-                    "zoomIn2d",
-                    "zoomOut2d",
-                    "resetScale2d",
-                    "toggleSpikelines",
-                    "hoverCompareCartesian",
-                    "hoverClosestCartesian",
-                ],
-            }
-        )
-
-    else:
-        return fig
-
-
-def model_plot_shap_lines_for_variables(
-    full_result,
-    timestamps,
-    event_inds,
-    event_times,
-    variables,
-    to_discretize,
-    discretization_method,
-    n_bins,
-    x_range=None,
-    kind="default",
-    show_risk_table=True,
-    show=True,
-    title=None,
-):
-
-    full_result_copy = full_result[
-        (full_result["variable_name"].isin(variables)) & (full_result["B"] == 0)
-    ].reset_index(drop=True)
-    if kind == "ratio":
-        full_result_copy.iloc[:, 5:] = (
-            full_result_copy.iloc[:, 5:] / full_result_copy.iloc[:, 5:].abs().sum()
-        )
-    for var in to_discretize:
-        if discretization_method == "quantile":
-            discretize_map = dict(
-                pd.qcut(
-                    full_result_copy.loc[
-                        full_result_copy["variable_name"] == var, "variable_value"
-                    ],
-                    q=n_bins,
-                ).astype(str)
-            )
-            full_result_copy.loc[
-                full_result_copy["variable_name"] == var, "variable_value"
-            ] = full_result_copy[full_result_copy["variable_name"] == var]["index"].map(
-                discretize_map
-            )
-            full_result_copy.loc[
-                full_result_copy["variable_name"] == var, "variable_value"
-            ] = (
-                "in "
-                + full_result_copy.loc[
-                    full_result_copy["variable_name"] == var, "variable_value"
-                ]
-            )
-
-    df_prepared_to_plot = (
-        full_result_copy.groupby(["variable_name", "variable_value"])
-        .mean(numeric_only=True)
-        .reset_index()
-    )
-
-    fig = plotly.subplots.make_subplots(
-        rows=2, cols=1, print_grid=False, shared_xaxes=True
-    )
-
-    max_y, min_y = -1, 1
-    keyword = "SHAP value: " if kind == "default" else "normalized SHAP value: "
-    for index, row in df_prepared_to_plot.iterrows():
-        x_vals = timestamps
-        y_vals = row[5:].values
-
-        new_x, new_y = insert_zeros_to_line(x_vals, y_vals)
-
-        fig.add_trace(
-            plotly.graph_objs.Scatter(
-                x=new_x,
-                y=new_y,
-                mode="lines",
-                line_color="#8bdcbe",
-                line_width=2,
-                hovertemplate="<b>"
-                + str(row["variable_name"])
-                + "</b><br>"
-                + "Variable value: "
-                + str(row["variable_value"])
-                + "<br>"
-                "Time: %{x}<br>" + keyword + "%{y:.6f}<extra></extra>",
-                hoverinfo="text",
-            ),
-            row=1,
-            col=1,
-        )
-        fig.add_trace(
-            plotly.graph_objs.Scatter(
-                x=new_x[new_y <= 0],
-                y=new_y[new_y <= 0],
-                mode="lines",
-                line_color="#f05a71",
-                line_width=2,
-                hovertemplate="<b>"
-                + str(row["variable_name"])
-                + "</b><br>"
-                + "Variable value: "
-                + str(row["variable_value"])
-                + "<br>"
-                "Time: %{x}<br>" + "SF change: " + "%{y:.6f}<extra></extra>",
-                hoverinfo="text",
-            ),
-            row=1,
-            col=1,
-        )
-
-        max_y, min_y = check_y_range(max_y, min_y, y_vals)
-
-    min_y = min_y - 0.05 * abs(min_y)
-    max_y = max_y + 0.05 * abs(max_y)
-
-    if x_range is None:
-        x_range = [min(timestamps) - max(timestamps) * 0.025, max(timestamps) * 1.025]
-
-    ticker = matplotlib.ticker.MaxNLocator(
-        nbins=10, min_n_ticks=4, integer=True, prune="upper"
-    )
-    ticks = ticker.tick_values(x_range[0], x_range[1])
-
-    if show_risk_table:
-        n_at_risk, n_events, n_censored = calculate_risk_table(
-            ticks, event_times, event_inds
-        )
-        fig.append_trace(
-            plotly.graph_objs.Scatter(
-                x=ticks,
-                y=[0.8] * len(ticks),
-                text=n_at_risk,
-                mode="text",
-                showlegend=False,
-            ),
-            2,
-            1,
-        )
-        fig.append_trace(
-            plotly.graph_objs.Scatter(
-                x=ticks,
-                y=[0.5] * len(ticks),
-                text=n_events,
-                mode="text",
-                showlegend=False,
-            ),
-            2,
-            1,
-        )
-        fig.append_trace(
-            plotly.graph_objs.Scatter(
-                x=ticks,
-                y=[0.2] * len(ticks),
-                text=n_censored,
-                mode="text",
-                showlegend=False,
-            ),
-            2,
-            1,
-        )
-
-    fig.update_xaxes(
-        {
-            "matches": None,
-            "showticklabels": True,
-            "title": "time",
-            "title_standoff": 0,
-            "type": "linear",
-            "gridwidth": 2,
-            "zeroline": False,
-            "automargin": True,
-            "tickmode": "array",
-            "tickvals": ticks,
-            "ticktext": np.around(ticks, 2),
-            "tickcolor": "white",
-            "ticklen": 3,
-            "fixedrange": True,
-            "range": x_range,
-        }
-    ).update_yaxes(
-        {
-            "type": "linear",
-            "gridwidth": 2,
-            "zeroline": False,
-            "automargin": True,
-            "ticks": "outside",
-            "tickcolor": "white",
-            "ticklen": 3,
-            "fixedrange": True,
-            "range": [min_y, max_y],
-        }
-    ).update_layout(
-        {"showlegend": False, "template": "none", "margin_pad": 6, "margin_l": 110}
-    ).update_layout(
-        yaxis2={
-            "tickvals": [0.2, 0.5, 0.8],
-            "ticktext": ["censored", "events", "at risk"],
-            "showgrid": False,
-            "domain": [0.0, 0.2],
-            "range": [0, 1],
-        },
-        xaxis2={"visible": False},
-    )
-
     if kind == "ratio":
         fig.update_layout(
             yaxis1={"domain": [0.35, 1], "title": "normalized SHAP value"},
