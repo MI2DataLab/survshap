5a42c361aa9a76d5b00b57f13418c77cd22c3873
krzyzinskim <mateusz_krzyzinski@wp.pl>
2023-10-27 23:57:15 +0200

    bump version

--- survshap_package/survshap/__init__.py
+++ survshap_package/survshap/__init__.py
@@ -2,7 +2,6 @@ from .predict_explanations.object import PredictSurvSHAP
 from .model_explanations.object import ModelSurvSHAP
 from .explainer import SurvivalModelExplainer
 
-__version__ = "0.4.0"
-
+__version__ = "0.4.1"
 
 __all__ = ["PredictSurvSHAP", "ModelSurvSHAP", "SurvivalModelExplainer"]
